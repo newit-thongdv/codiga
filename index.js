ac6a43ee716245be4272a6ac407689e8fb3b269d
ThongDV <thongdv@newit.co.jp>
2025-03-18 21:35:52 +0700

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import open from 'open';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import mysql from 'mysql2';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,6 +15,27 @@ app.use(express.static(publicPath));
 
 const port = 3030;
 
+const dbConfig = {
+  host: 'mysql',
+  port: 3306,
+  user: 'root',
+  password: 'admin',
+  database: 'codiga',
+};
+
+let connection;
+
+
+async function connectDB() {
+  try {
+    connection = await mysql.createConnection(dbConfig);
+    console.log('✅ MySQL connected successfully!');
+  } catch (error) {
+    console.error('❌ MySQL connection failed:', error);
+    process.exit(1); // Dừng server nếu không kết nối được
+  }
+}
+
 router.use((req, res, next) => {
   console.log('/' + req.method);
   next();
@@ -25,7 +47,12 @@ router.get('/', (req, res) => {
 
 app.use('/', router);
 
-app.listen(port, async () => {
-  console.log(`Server running at http://localhost:${port}`);
-  await open(`http://localhost:${port}`);
-});
+async function startServer() {
+  await connectDB();
+  app.listen(port, async () => {
+    console.log(`🚀 Server running at http://localhost:${port}`);
+    await open(`http://localhost:${port}`);
+  });
+}
+
+startServer();
